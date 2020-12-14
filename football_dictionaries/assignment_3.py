e7750e4a434a386d7d56ab912708f4b726f266c3
lewandzio <74707963+lewandzio@users.noreply.github.com>
2020-12-14 12:19:04 +0100

    Question for validation - task 3

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,11 @@
 def players_by_country_and_position(squads_list):
-    pass
+    result_all = {}
+    for cou in squads_list:
+        country = cou[6]
+        result = {}
+        result_all.setdefault(country, result)
+        for pos in squads_list:
+            position = pos[1]
+            result.setdefault(position, [])
+            result[position].append(pos[2])
+    return result_all
